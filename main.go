ce6849699f75e6b8487de82a1efea3a236252c95
greenthepear <green@greenthepear.com>
2024-08-13 12:23:45 +0200

    CLI polished

--- main.go
+++ main.go
@@ -3,49 +3,58 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"os"
 )
 
+func fatalErrorCheck(err error, whileDoingWhat string) {
+	if err != nil {
+		log.Fatalf("Error while %s:\n\t%v", whileDoingWhat, err)
+	}
+}
+
 func main() {
-	in := flag.String("in", "", "input markdown file")
-	repoLink := flag.String("repo", "", "link to the repository: e.g github.com/exampleguy/example")
+	in := flag.String(
+		"in", "",
+		"input file, default will scan from stdin")
+	repoLink := flag.String(
+		"repo", "",
+		"link to the repository: e.g github.com/exampleguy/example")
 	out := flag.String(
 		"out", "",
-		"output file, not setting it will ask to confirm overwriting, use -o to not ask")
+		"output file, not setting it or -ow will just print the new text to stdout")
 	overwrite := flag.Bool(
 		"ow", false,
-		"overwrtie the original file, will ignore -out and not bug you to confirm overwrite if output no provided")
-
+		"overwrtie the original file, will ignore -out")
 	flag.Parse()
-	if *in == "" {
-		log.Fatalf("No input file provided.")
-	}
+
 	if *repoLink == "" {
-		log.Fatalf("No repo link provided, add flag like:\n\tlinktime -repo \"github.com/greenthepear/linktime\"")
+		log.Fatalf("No repo link provided, add flag like:\n\tglin -repo \"github.com/greenthepear/glin\"")
 	}
 
-	text, err := os.ReadFile(*in)
-	if err != nil {
-		log.Fatalf("Error while reading file: %v", err)
+	var text []byte
+	var err error
+	if *in == "" {
+		text, err = io.ReadAll(os.Stdin)
+		fatalErrorCheck(err, "reading from stdin")
+	} else {
+		text, err = os.ReadFile(*in)
+		fatalErrorCheck(err, "reading file")
 	}
+
 	changed := AddLinks(string(text), *repoLink)
-	if *out == "" {
-		if !*overwrite {
-			fmt.Printf("Do you want to overwrite %s? (y/N)\n", *in)
-			choice := "N"
-			_, err := fmt.Scan(&choice)
-			if err != nil {
-				log.Fatalf("Bad input.")
-			}
-			if choice != "Y" && choice != "y" {
-				return
-			}
-		}
-		err := os.WriteFile(*in, []byte(changed), 0644)
-		if err != nil {
-			log.Fatalf("Error while writing file: %v", err)
-		}
+	if *overwrite {
+		err = os.WriteFile(*in, []byte(changed), 0644)
+		fatalErrorCheck(err, "overwriting file")
+		return
 	}
-
+	if *out == "" {
+		fmt.Print(changed)
+		return
+	}
+	err = os.WriteFile(*out, []byte(changed), 0644)
+	if err != nil {
+		fatalErrorCheck(err, "writing to file")
+	}
 }
